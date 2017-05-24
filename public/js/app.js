7c8f6f3d58a55f69bb13ad6d94b9a8a214a0157a
Letty Fuentes <lettyfuentes2013@gmail.com>
2017-05-23 23:07:13 -0500

    Static version of the editable attraction list

--- public/js/app.js
+++ public/js/app.js
@@ -1,92 +1,170 @@
-class AttractionList extends React.Component {
-    state = {
-        attractions: [],
-    };
-
-    componentDidMount() {
-        this.setState({ attractions: Seed.attractions });
-    }
-
-    _handleAttractionUpVote = (attractionId) => {
-        const nextAttractions = this.state.attractions.map((attraction) => {
-            if(attraction.id === attractionId) {
-                return Object.assign({}, attraction, {
-                    votes: attraction.votes + 1,
-                });
-            } else {
-                return attraction;
-            }
-        });
-        this.setState({
-            attractions: nextAttractions,
-        });        
-    }
-
+class AttractionsDashboard extends React.Component {
     render() {
-        const attractions = this.state.attractions.sort((a, b) => (b.votes - a.votes));
-        const attractionComponents = attractions.map((attraction) => (
-            <Attraction
-                key={"attraction-" + attraction.id}
-                id={attraction.id}
-                title={attraction.title}
-                description={attraction.description}
-                url={attraction.url}
-                attractionImageUrl={attraction.attractionImageUrl}
-                votes={attraction.votes}
-                lastVoteAvatarUrl={attraction.lastVoteAvatarUrl}
-                lastVoteName={attraction.lastVoteName}
-                onVote={this._handleAttractionUpVote}
-            />   
-        ));
         return (
-            <div className="ui unstackable items">
-                {attractionComponents}
+            <div>
+                <EditableAttractionList />
+                <ToggleableAttractionForm 
+                    isOpen={false}
+                />
+            </div>           
+        );
+    }
+}
+
+class ToggleableAttractionForm extends React.Component {
+    render() {
+        if (this.props.isOpen) {
+            return (
+                <AttractionForm />
+            );
+        } else {
+            return (
+                <div className="ui basic content center aligned segment">
+                    <button className="ui basic button icon">
+                        <i className="plus icon" />
+                    </button>
+                </div>
+            );
+        }        
+    }
+}
+
+class EditableAttractionList extends React.Component {
+    render() {
+        return (
+            <div id="attractions" className="ui divided items">
+                <EditableAttraction
+                    key="1"
+                    attractionId="1"
+                    attractionName="Sea World"
+                    attractionDescription="Sea World Description"
+                    attractionUrl="https://seaworld.com/san-antonio/"
+                    attractionImageUrl="images/attractions/image-seaworldsalogo.png"
+                    attractionVoteCount="32"
+                    attractionLastVoteAvatarUrl="images/avatars/veronika.jpg"
+                    attractionLastVoteName="Kristy"
+                    editFormOpen={true}
+                />
+
+                <EditableAttraction
+                    key="2"
+                    attractionId="2"
+                    attractionName="SA Zoo"
+                    attractionDescription="SA Zoo Description"
+                    attractionUrl="https://sazoo.org/"
+                    attractionImageUrl="images/attractions/image-sanantoniozoo.png"
+                    attractionVoteCount="45"
+                    attractionLastVoteAvatarUrl="images/avatars/daniel.jpg"
+                    attractionLastVoteName="Daniel"
+                    editFormOpen={false}
+                />
+            </div>
+        );
+    }
+}
+
+class EditableAttraction extends React.Component {
+    render() {
+        if (this.props.editFormOpen) {
+            return (
+                <AttractionForm
+                    attractionName={this.props.attractionName}
+                    attractionDescription={this.props.attractionDescription}
+                    attractionUrl={this.props.attractionUrl}
+                    attractionImageUrl={this.props.attractionImageUrl}
+                />
+            );
+        } else {
+            return (
+                <Attraction 
+                    attractionName={this.props.attractionName}
+                    attractionDescription={this.props.attractionDescription}
+                    attractionUrl={this.props.attractionUrl}
+                    attractionImageUrl={this.props.attractionImageUrl}
+                    attractionVoteCount={this.props.attractionVoteCount}
+                    attractionLastVoteAvatarUrl={this.props.attractionLastVoteAvatarUrl}
+                    attractionLastVoteName={this.props.attractionLastVoteName}
+                />
+            );
+        }
+    }
+}
+
+class AttractionForm extends React.Component {
+    render () {
+        const submitText = this.props.attractionName ? "Update" : "Create";
+        return (
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="ui form">
+                        <div className="field">
+                            <label>Attraction Name</label>
+                            <input type="text" defaultValue={this.props.attractionName} />
+                        </div>
+                        <div className="field">
+                            <label>Attraction Description</label>
+                            <input type="text" defaultValue={this.props.attractionDescription} />
+                        </div>
+                        <div className="field">
+                            <label>Attraction Website</label>
+                            <input type="text" defaultValue={this.props.attractionUrl} />
+                        </div>
+                        <div className="field">
+                            <label>Attraction Image</label>
+                            <input type="text" defaultValue={this.props.attractionImageUrl} />
+                        </div>
+                        <div className="ui two bottom attached buttons">
+                            <button className="ui basic blue button">
+                                {submitText}
+                            </button>
+                            <button className="ui basic red button">
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
 class Attraction extends React.Component {
-    _handleUpVote = () => (
-        this.props.onVote(this.props.id)
-    );
-
     render() {
         return(
             <div className="item">
-                <div className="image">
+                <div className="ui small image">
                     <img src={this.props.attractionImageUrl} />
                 </div>
                 <div className="middle aligned content">
                     <div className="header">
-                        <a onClick={this._handleUpVote}>
+                        <a >
                             <i className="large caret up icon" />
                         </a>
-                        {this.props.votes}
+                        {this.props.attractionVoteCount}
                     </div>
                     <div className="description">
-                        <a href={this.props.url}>
-                            {this.props.title}
+                        <a href={this.props.attractionUrl}>
+                            {this.props.attractionName}
                         </a>
                         <p>
-                            {this.props.description}
+                            {this.props.attractionDescription}
                         </p>
                     </div>
                     <div className="extra">
                         <span>Last vote by:</span>
                         <img
                             className="ui avatar image"
-                            src={this.props.lastVoteAvatarUrl}
+                            src={this.props.attractionLastVoteAvatarUrl}
                         />
-                        <span>{this.props.lastVoteName}</span>
+                        <span>{this.props.attractionLastVoteName}</span>
                     </div>
                 </div>
-            </div>
+            </div> 
         );
     }
 }
 
 ReactDOM.render(
-    <AttractionList />,
+    <AttractionsDashboard />,
     document.getElementById("content")
 );
